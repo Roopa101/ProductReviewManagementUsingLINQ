688c657ab6d670025ee58a26de301d568a5b6177
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-05 17:10:45 +0530

    [Roopa] Added: Average Rating Of The Each ProductID Using LINQ

--- ProductManagementUsingLINQ/ProductManagementUsingLINQ/Management.cs
+++ ProductManagementUsingLINQ/ProductManagementUsingLINQ/Management.cs
@@ -77,7 +77,15 @@ namespace ProductManagementUsingLINQ
                 Console.WriteLine("ProductId : " + Product.Field<int>("ProductID") + "\t" + "UserID : " + Product.Field<int>("UserID") + "\t" + "Rating : " + Product.Field<int>("Rating") + "\t" + "Review : " + Product.Field<string>("Review") + "\t" + "ISLike : " + Product.Field<bool>("ISLike"));
             }
         }
-
+        public void AverageProductID(List<ProductReview> productList)
+        {
+            var records = productList.GroupBy(X => X.ProductID).Select(X => new { ProductID = X.Key, AverageRating = X.Average(x => x.Rating) });
+            foreach (var data in records)
+            {
+                Console.WriteLine("ProductID "+data.ProductID + " =" + " Average "+ data.AverageRating);
+            }
+        }
     }
 }
-          
+
+             
--- ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
+++ ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
@@ -29,7 +29,7 @@ namespace ProductManagementUsingLINQ
             Management management = new Management();
             while (flag)
             {
-                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Retrieve Records using Product ID which is Rating > 3 \n 4. The Number of Recirds of ProductID \n 5. Retrieve Product ID and Review \n 6. Skip Top Five Records \n 7. Retrieve Product ID and Review \n 8. Product Review of Datatable \n 9. Retrieve Records From DataTable \n 10. Exit");
+                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Retrieve Records using Product ID which is Rating > 3 \n 4. The Number of Records of ProductID \n 5. Retrieve Product ID and Review \n 6. Skip Top Five Records \n 7. Retrieve Product ID and Review \n 8. Product Review of Datatable \n 9. Retrieve Records From DataTable \n 10. Average ProductID \n 11. Exit");
                 int number = Convert.ToInt32(Console.ReadLine());
                 switch (number)
                 {
@@ -62,6 +62,9 @@ namespace ProductManagementUsingLINQ
                         management.RetrieveRecordsFromDataTable(productList);
                         break;
                     case 10:
+                        management.AverageProductID(productList);
+                        break;
+                    case 11:
                         flag = false;
                         break;
                 }
