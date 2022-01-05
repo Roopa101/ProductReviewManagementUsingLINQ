c028e7496eedafbf862a553fc53beb36e0595571
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-05 13:27:39 +0530

    [Roopa] Added: Displayed Welcome to Product Review Management Program

---
+++ ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementUsingLINQ
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Using LINQ Program ");
+            Console.ReadLine();
+        }
+    }
+}
