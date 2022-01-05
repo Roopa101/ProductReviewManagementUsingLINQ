a181ed2d01ee8dcc282ebee3f2c06f375a3b153c
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-05 14:07:45 +0530

    [Roopa] Modified: Retrived All Records from the list who's rating are greater then 3 and ProductID is 1 or 4 or 9 using LINQ

--- ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
+++ ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
@@ -29,7 +29,7 @@ namespace ProductManagementUsingLINQ
             Management management = new Management();
             while (flag)
             {
-                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Exit");
+                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Retrieve Records using Product ID which is Rating > 3 \n 4. Exit");
                 int number = Convert.ToInt32(Console.ReadLine());
                 switch (number)
                 {
@@ -40,6 +40,9 @@ namespace ProductManagementUsingLINQ
                         management.TopThreeRecords(productList);
                         break;
                     case 3:
+                        management.RetrieveRecordsByProductID(productList);
+                        break;
+                    case 4:
                         flag = false;
                         break;
                 }
