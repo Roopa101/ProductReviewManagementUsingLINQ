db62c91cef19ba12e1a56bb8964d760ba4d6b394
Roopa Jamadar <roopajamadar14@gmail.com>
2022-01-05 13:55:01 +0530

    [Roopa] Added: Retrived Top three Records From the list Who's rating is high using LINQ

--- ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
+++ ProductManagementUsingLINQ/ProductManagementUsingLINQ/Program.cs
@@ -14,22 +14,22 @@ namespace ProductManagementUsingLINQ
 
             List<ProductReview> productList = new List<ProductReview>
             {
-                new ProductReview(){ProductID=1 ,  UserID = 1, Rating = 5, Review = "Excellent",ISLike = true},
-                new ProductReview(){ProductID=2  , UserID = 2, Rating = 4, Review ="VeryGood",  ISLike=true},
-                new ProductReview(){ProductID=3 ,  UserID = 3, Rating = 3, Review="Good",       ISLike=true},
-                new ProductReview(){ProductID=4 ,  UserID = 4, Rating = 2, Review="Bad",        ISLike=false},
-                new ProductReview(){ProductID=5 ,  UserID = 5, Rating = 1, Review="VeryBad",    ISLike=false},
-                new ProductReview(){ProductID=6 ,  UserID = 6, Rating = 1, Review="VeryBad",    ISLike=false},
-                new ProductReview(){ProductID=7 ,  UserID = 7, Rating = 2, Review="Bad",        ISLike=false},
-                new ProductReview(){ProductID=8 ,  UserID = 8, Rating = 3, Review="Good",       ISLike=true},
-                new ProductReview(){ProductID=9 ,  UserID = 9, Rating = 4, Review="VeryGood",   ISLike=true},
-                new ProductReview(){ProductID=10 , UserID = 10, Rating = 5, Review="Excellent", ISLike=true}
+                new ProductReview(){ProductID=1, UserID=1,Rating=5,Review="Excellent",ISLike=true},
+                new ProductReview(){ProductID=2, UserID=2,Rating=4,Review="VeryGood",ISLike=true},
+                new ProductReview(){ProductID=3, UserID=3,Rating=3,Review="Good",ISLike=true},
+                new ProductReview(){ProductID=4, UserID=4,Rating=2,Review="Bad",ISLike=false},
+                new ProductReview(){ProductID=5, UserID=5,Rating=1,Review="VeryBad",ISLike=false},
+                new ProductReview(){ProductID=6, UserID=6,Rating=1,Review="VeryBad",ISLike=false},
+                new ProductReview(){ProductID=7, UserID=7,Rating=2,Review="Bad",ISLike=false},
+                new ProductReview(){ProductID=8, UserID=8,Rating=3,Review="Good",ISLike=true},
+                new ProductReview(){ProductID=9, UserID=9,Rating=4,Review="VeryGood",ISLike=true},
+                new ProductReview(){ProductID=10, UserID=10,Rating=5,Review="Excellent",ISLike=true}
             };
             bool flag = true;
             Management management = new Management();
             while (flag)
             {
-                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Exit");
+                Console.WriteLine("Specify the Number To Excute USE CASE Wise Problems:-- \n 1. Displaying ADD List \n 2. Top Three Records According to Rating \n 3. Exit");
                 int number = Convert.ToInt32(Console.ReadLine());
                 switch (number)
                 {
@@ -37,13 +37,14 @@ namespace ProductManagementUsingLINQ
                         management.Display(productList);
                         break;
                     case 2:
+                        management.TopThreeRecords(productList);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
             }
             Console.ReadLine();
-
         }
     }
 }
-     
